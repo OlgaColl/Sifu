17416ac5444cf385950493243515b04d801c2961
OlgaColl <ocoll94@gmail.com>
2017-05-04 16:34:09 +0200

    Create activities, layouts and string file

---
+++ app/src/main/java/com/example/olgacoll/sifu/ConfigActivity.java
@@ -0,0 +1,17 @@
+package com.example.olgacoll.sifu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by olgacoll on 4/5/17.
+ */
+
+public class ConfigActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+}
